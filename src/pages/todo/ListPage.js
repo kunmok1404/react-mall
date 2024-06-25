9fbda2a54557c808d7576567ae00d4b020ea5949
박근목 <tedy@baggeunmog-ui-MacBookAir.local>
2024-06-25 21:20:20 +0900

    20240625_2

--- src/pages/todo/ListPage.js
+++ src/pages/todo/ListPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ListComponent from "../../components/todo/ListComponent";
 
 function ListPage(props) {
   return (
     <div className="p-4 w-full bg-orange-200 ">
       <div className="text-3xl font-extrabold">Todo List Page Component</div>
+      <ListComponent />
     </div>
   );
 }
